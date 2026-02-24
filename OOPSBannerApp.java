0b1639db73e5e27d4af65070f8cee305b850b377
PalakThitwal <palakthitwal@gmail.com>
2026-02-24 08:19:32 +0530

    Coded to Render OOPS as Banner using String join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,40 @@
+
 /**
-* OOPSBannerApp UC1 - OOPS Banner Display Application
-*
-*This class demonstrates a simple Java Application that displays the Object
-*Orientd Programming System OOPS acronym to the console.
-*
-* @PalakThitwal Developer
-* @version 1.0
+@PalakThitwal Developer
+@version 2
 */
 
-public class OOPSBannerApp{
-	
-	public static void main(String[] args) {
+/**
+public class OOPSBannerApp {
+	public static void main(String[] args)
+	{
 		System.out.println("OOPS");
+		System.out.println("   ***   " + "   ***   " + "   ******   " + "   ******   ");
+		System.out.println(" **   ** " + " **   ** " + " **      ** " + " ***   ");
+		System.out.println(" **   ** " + " **   ** " + " **      ** " + "  ***   ");
+		System.out.println(" **   ** " + " **   ** " + " **      ** " + "    *****   ");
+		System.out.println(" **   ** " + " **   ** " + " *******    " + "       ***   ");
+		System.out.println(" **   ** " + " **   ** " + " **         " + "       ***   ");
+		System.out.println("   ***   " + "   ***   " + " **         " + "   ******   ");
 	}
 }
+*/
+
+/**
+@PlakThitwal Developer
+@version 3.0
+*/
+
+public class OOPSBannerApp {
+		public static void main(String[] args)
+		{
+			System.out.println(String.join(" " , "   ***   " , "   ***   " , "   ******   " , "   ******   "));
+			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , " ***        "));
+			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "  ***       "));
+			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **      ** " , "    *****   "));
+			System.out.println(String.join(" " , " **   ** " , " **   ** " , " *******    " , "       ***  "));
+			System.out.println(String.join(" " , " **   ** " , " **   ** " , " **         " , "       ***  "));
+			System.out.println(String.join(" " , "   ***   " , "   ***   " , " **         " , "   ******   "));
+		}
+}
+		
